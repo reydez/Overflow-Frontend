f4e7dd273e3b19d1342c7af46e25a29d9c86fadc
lisandroman <lisandromansilla.dg@gmail.com>
2022-05-25 18:32:25 +0300

    Preguntas en la home

--- src/components/Avatars/Avatars.jsx
+++ src/components/Avatars/Avatars.jsx
@@ -1,16 +1,15 @@
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import { yellow, deepPurple, green, red, blue } from '@mui/material/colors';
 import { Button, ButtonGroup } from '@mui/material';
 
 export default function Avatars() {
   return (
     <Stack direction="row" spacing={2}>
     <ButtonGroup variant="container" arial-label="container button group" sx={{bgcolor: "transparent"}}>
-      <Button size="large" ><Avatar size="small" sx={{ bgcolor: deepPurple[700], color: yellow[500] }}>M1</Avatar></Button>
-      <Button><Avatar sx={{ bgcolor: deepPurple[700], color: green[500] }}>M2</Avatar></Button>
-      <Button><Avatar sx={{ bgcolor: deepPurple[700], color: red[300] }}>M3</Avatar></Button>
-      <Button><Avatar sx={{ bgcolor: deepPurple[700], color: blue[300] }}>M4</Avatar></Button>
+      <Button size="large" ><Avatar size="small" sx={{ bgcolor: '#3A2D53', color: '#FBC02D' }}>M1</Avatar></Button>
+      <Button><Avatar sx={{ bgcolor: '#3A2D53', color: '#43A047' }}>M3</Avatar></Button>
+      <Button><Avatar sx={{ bgcolor: '#3A2D53', color: '#D81B60' }}>M2</Avatar></Button>
+      <Button><Avatar sx={{ bgcolor: '#3A2D53', color: '#42A5F5' }}>M4</Avatar></Button>
       </ButtonGroup>
     </Stack>
   );
