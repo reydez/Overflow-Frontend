11877ecde52f1e04e6933467061d5f09238b9709
matias199130 <matiasromero20121991@gmail.com>
2022-05-25 18:59:28 -0300

    primer commit de landing

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,5 +1,9 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return <div>LandingPage
+  <Link to="/questions"><Button >HOME</Button></Link>
+  </div>;
 }
--- src/views/VisualizeQuestion.jsx
+++ src/views/VisualizeQuestion.jsx
@@ -15,12 +15,14 @@ export default function VisualizeQuestion() {
 
     loadQuestionDetails();
   }, [dispatch, param]);
+
+
   return (
     <div>
       <h2>{question.title}</h2>
       <h4>{question.message}</h4>
-      {question.comments.length > 0 ? (
-        question.comments.map((comment, index) => (
+      {question.comments?.length > 0 ? (
+        question.comments?.map((comment, index) => (
           <div key={index}>
             <p>User id: {comment.id_user}</p>
             <p>User comment: {comment.message}</p>
