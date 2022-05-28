2eb13866292b82ed88e20ac8e04ff7a61011f5a6
reydez <icyreyeslod@gmail.com>
2022-05-28 15:00:30 -0500

    Post details UI

--- src/views/VisualizeQuestion.jsx
+++ src/views/VisualizeQuestion.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import DetailsComponent from "../components/DetailsComponent/DetailsComponent";
 import { getQuestionDetails } from "../redux/actions/questionsActions";
-
 
 export default function VisualizeQuestion() {
   const { questionId } = useParams();
@@ -20,24 +20,5 @@ export default function VisualizeQuestion() {
     loadQuestionDetails();
   }, [dispatch, questionId]);
 
-  return (
-    <div>
-      <h2>{question.title}</h2>
-      <h4>{question.message}</h4>
-      {loading && <h3>Loading Question Details...</h3>}
-      {question.comments?.length > 0 ? (
-        question.comments.map((comment, index) => (
-          <div key={index}>
-            <p>
-              User name:{" "}
-              {`${question.user.first_name} ${question.user.last_name}`}
-            </p>
-            <p>User comment: {comment.message}</p>
-          </div>
-        ))
-      ) : (
-        <h3>esta pregunta no tiene comentarios</h3>
-      )}
-    </div>
-  );
+  return <DetailsComponent question={question} loading={loading} />;
 }
