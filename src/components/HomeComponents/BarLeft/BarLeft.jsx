b7d4398a12a8cb4edf598ccaf6119fcd15a643f4
matias199130 <matiasromero20121991@gmail.com>
2022-05-30 13:31:59 -0300

    ultimo commit para demo 1

--- src/components/HomeComponents/BarLeft/BarLeft.jsx
+++ src/components/HomeComponents/BarLeft/BarLeft.jsx
@@ -12,6 +12,7 @@ import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import { Questions } from "../Questions/Questions.jsx";
+import { Link } from "react-router-dom";
 
 /* aqui va el componente de lisandro search  */
 
@@ -90,7 +91,19 @@ export default function BarLeft(props) {
             <MenuIcon />
           </IconButton>
           {/* <Avatar /> */}
-          <Button /* disableRipple  */sx={{ marginLeft:'30px', borderRadius:'20px', color: '#413a66', fontSize: '12px', padding: '5px 10px', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', boxShadow: '0 3px 5px 2px rgba(255,105,135, .3)' }}>Hacer una pregunta</Button>
+          <Link to={"/create-question"}>
+          <Button /* disableRipple  */
+              sx={{
+                marginLeft:'30px',
+                borderRadius:'20px',
+                color: '#413a66',
+                fontSize: '12px',
+                padding: '5px 10px',
+                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', 
+                boxShadow: '0 3px 5px 2px rgba(255,105,135, .3)' 
+                }}>
+                Hacer una pregunta</Button>
+          </Link>
           <SearchBar />
         </Toolbar>
       </AppBar>
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -48,12 +48,20 @@ function LandingPage() {
       <Typography variant="text" style={{ color: "#121858" }}></Typography>
       <Link to="/intro-user">
   
-          <Chip color="error" label="Ingrese" size="large"
+          <Chip color="error" label="Login" size="large"
              style={{
-            position: "absolute",
-            top: "500px",
-            left: "200px",
-            cursor: "pointer"
+              marginLeft:'30px', 
+              borderRadius:'20px', 
+              color: '#222831',
+              size: 'large',
+              fontSize: '20px', 
+              padding: '5px 10px', 
+              background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', 
+              boxShadow: '0 3px 5px 2px rgba(255,105,135, .3)',
+              position: "absolute",
+              top: "500px",
+              left: "200px",
+              cursor: "pointer"
           }} />
       </Link>
       
