5009cbddf4b982540266875182085ccfb00638f0
reydez <icyreyeslod@gmail.com>
2022-06-04 14:22:45 -0500

    crear ususario en cuanto se logea y usuario puede comentar

--- src/components/HomeComponents/FormWithMUI/PostFormMui.jsx
+++ src/components/HomeComponents/FormWithMUI/PostFormMui.jsx
@@ -3,276 +3,239 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 
-import { postQuestion } from '../../../redux/actions/questionsActions'
+import { postQuestion } from "../../../redux/actions/questionsActions";
 
 import FormM1Tags from "./FormTags/FormM1Tags";
 import FormM2Tags from "./FormTags/FormM2Tags";
 import FormM3Tags from "./FormTags/FormM3Tags";
 import FormM4Tags from "./FormTags/FormM4Tags";
 
-
 import Classes from "./PostFormMui.module.css";
 import InputForm from "./StylesForm/InputForm";
 import { NameDiv } from "./StylesForm/styles";
 import InputFormArea from "./StylesForm/InputFormArea";
 
-
 const PostFormMui = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  //! ------------------------- CheckBoxes-----------------------
+  const [moduleSelected, setModuleSelected] = useState("selectModule");
 
-    //! ------------------------- CheckBoxes-----------------------
-    const [moduleSelected, setModuleSelected] = useState("selectModule");
+  const [m1TagsSelected, setM1TagsSelected] = useState(false);
+  const [m2TagsSelected, setM2TagsSelected] = useState(false);
+  const [m3TagsSelected, setM3TagsSelected] = useState(false);
+  const [m4TagsSelected, setM4TagsSelected] = useState(false);
 
-    const [m1TagsSelected, setM1TagsSelected] = useState(false)
-    const [m2TagsSelected, setM2TagsSelected] = useState(false)
-    const [m3TagsSelected, setM3TagsSelected] = useState(false)
-    const [m4TagsSelected, setM4TagsSelected] = useState(false)
+  const handleOnChange = (e) => {
+    setModuleSelected(e.target.value);
+    // console.log('Este es el name', e.target.name, 'Este es el value', e.target.value)
 
+    setModulo({
+      ...modulo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleOnChange = (e) => {
-        setModuleSelected(e.target.value);
-        // console.log('Este es el name', e.target.name, 'Este es el value', e.target.value)
+  const makeCheckboxesModule = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
-        setModulo({
-            ...modulo,
-            [e.target.name]: e.target.value
+  const renderResult = () => {
+    let result;
+    moduleSelected === "selectModule"
+      ? (result = "")
+      : (result = makeCheckboxesModule(moduleSelected));
+    return result;
+  };
+
+  useEffect(() => {
+    moduleSelected === "m1"
+      ? setM1TagsSelected(true)
+      : setM1TagsSelected(false);
+    moduleSelected === "m2"
+      ? setM2TagsSelected(true)
+      : setM2TagsSelected(false);
+    moduleSelected === "m3"
+      ? setM3TagsSelected(true)
+      : setM3TagsSelected(false);
+    moduleSelected === "m4"
+      ? setM4TagsSelected(true)
+      : setM4TagsSelected(false);
+  }, [moduleSelected]);
+  //! ------------------------- CheckBoxes-----------------------
+
+  //? ------------------------- FORM - 'NUEVA IMPLEMENTACIÓN' -----------------------
+
+  const [title, setTitle] = useState({ field: "", validate: null });
+  const [description, setDescription] = useState({ field: "", validate: null });
+  const [code, setCode] = useState({ field: "", validate: null });
+  const [modulo, setModulo] = useState({ field: "" });
+  const [tag, setTag] = useState({ tags: [] });
+
+  const [validate, setValidate] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!tag.tags.length > 2) {
+      console.log("tienes más de 3");
+      alert("Debes elegir menos de 3 tags");
+    } else if (
+      title.validate === "true" &&
+      description.validate === "true" &&
+      code.validate === "true"
+      // modulo.validate === 'true' &&
+    ) {
+      setValidate(true);
+      dispatch(
+        postQuestion({
+          title: title.field,
+          description: description.field,
+          code: code.field,
+          modulo: modulo.field,
+          tag: tag.tags,
         })
+      );
+    } else {
+      setValidate(false);
     }
+  };
 
-    const makeCheckboxesModule = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1)
-    }
+  console.log("Estos son los tags:", tag.tags);
+  console.log("Esto es el title:", title.field);
+  console.log("Esta es la img code:", code.field);
+  console.log("Modulo:", modulo.field);
+  console.log("Esta es la descripción:", description.field);
 
-    const renderResult = () => {
-        let result;
-        moduleSelected === "selectModule"
-            ? (result = "")
-            : (result = makeCheckboxesModule(moduleSelected))
-        return result;
-    }
+  // console.log("Esto es el title:", title.validate)
+  // console.log("Estos son los tags:", tag.validate)
+  // console.log("Esta es la img code:", code.validate)
+  // console.log("Modulo:", modulo.validate)
+  // console.log("Esta es la descripción:", description.validate)
+  return (
+    <div className={Classes.layout}>
+      <Formulario onSubmit={handleSubmit}>
+        <div className={Classes.container}>
+          <h1 className={Classes.h1}> Pregúntame algo... </h1>
+          <NameDiv>
+            <InputForm
+              type="text"
+              state={title}
+              changeState={setTitle}
+              name="title"
+              placeholder="Pregunta..."
+              label="Pregunta"
+              error="Tu pregunta debe llevar más de 10 caracteres."
+              regularExpresion={/^[a-zA-ZÀ-ÿ\s?.,0-9]{4,100}$/}
+            />
+          </NameDiv>
 
-    useEffect(() => {
-        moduleSelected === "m1" ? setM1TagsSelected(true) : setM1TagsSelected(false);
-        moduleSelected === "m2" ? setM2TagsSelected(true) : setM2TagsSelected(false);
-        moduleSelected === "m3" ? setM3TagsSelected(true) : setM3TagsSelected(false);
-        moduleSelected === "m4" ? setM4TagsSelected(true) : setM4TagsSelected(false);
-    }, [moduleSelected])
-    //! ------------------------- CheckBoxes-----------------------
+          <InputForm
+            // type="url"
+            state={code}
+            changeState={setCode}
+            name="code"
+            placeholder="Imagen de tu código..."
+            label="Imagen de código"
+            error="Muéstranos una imagen sobre tu problema."
+            regularExpresion={/(https?:\/\/.*\.(?:png|jpg))/i}
+          />
+          <InputFormArea
+            // type="textarea"
+            state={description}
+            changeState={setDescription}
+            name="description"
+            placeholder="Describe tu pregunta..."
+            label="Descripción"
+            error="Háblanos sobre tu problema"
+            regularExpresion={/^[a-zA-ZÀ-ÿ\s?.,0-9]{20,400}$/}
+          />
+        </div>
 
-
-
-    //? ------------------------- FORM - 'NUEVA IMPLEMENTACIÓN' -----------------------
-
-    const [title, setTitle] = useState({ field: "", validate: null });
-    const [description, setDescription] = useState({ field: "", validate: null });
-    const [code, setCode] = useState({ field: "", validate: null });
-    const [modulo, setModulo] = useState({ field: "" });
-    const [tag, setTag] = useState({ tags: [] });
-
-    const [validate, setValidate] = useState(null);
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (!tag.tags.length > 2) {
-            console.log('tienes más de 3')
-            alert('Debes elegir menos de 3 tags');
-        }
-        else if (title.validate === 'true' &&
-            description.validate === 'true' &&
-            code.validate === 'true'
-            // modulo.validate === 'true' &&
-        ) {
-            setValidate(true)
-            dispatch(postQuestion({
-                title: title.field,
-                description: description.field,
-                code: code.field,
-                modulo: modulo.field,
-                tag: tag.tags
-            }))
-
-        } else {
-            setValidate(false)
-        }
-    }
-
-
-    console.log("Estos son los tags:", tag.tags)
-    console.log("Esto es el title:", title.field)
-    console.log("Esta es la img code:", code.field)
-    console.log("Modulo:", modulo.field)
-    console.log("Esta es la descripción:", description.field)
-
-    // console.log("Esto es el title:", title.validate)
-    // console.log("Estos son los tags:", tag.validate)
-    // console.log("Esta es la img code:", code.validate)
-    // console.log("Modulo:", modulo.validate)
-    // console.log("Esta es la descripción:", description.validate)
-    return (
-        <div className={Classes.layout}>
-
-
-
-
-            <Formulario
-
-                onSubmit={handleSubmit}
+        <div className={Classes.selectModules}>
+          <label>
+            Module selected{" "}
+            <span className={Classes.slec}>{renderResult()} </span>
+            <select
+              className={Classes.selectInput}
+              onChange={handleOnChange}
+              value={moduleSelected}
+              name="field"
             >
+              <option value={"selectModule"}>MODULES</option>
+              <option value={"m1"}>M1</option>
+              <option value={"m2"}>M2</option>
+              <option value={"m3"}>M3</option>
+              <option value={"m4"}>M4</option>
+            </select>
+          </label>
+        </div>
 
+        {m1TagsSelected && <FormM1Tags setTag={setTag} tag={tag} />}
+        {m2TagsSelected && <FormM2Tags setTag={setTag} tag={tag} />}
+        {m3TagsSelected && <FormM3Tags setTag={setTag} tag={tag} />}
+        {m4TagsSelected && <FormM4Tags setTag={setTag} tag={tag} />}
 
+        <button className={Classes.button} type="submit">
+          Enviar
+        </button>
 
-
-                <div className={Classes.container}>
-                    <h1 className={Classes.h1} > Pregúntame algo... </h1>
-                    <NameDiv>
-                        <InputForm
-                            type="text"
-                            state={title}
-                            changeState={setTitle}
-                            name="title"
-                            placeholder="Pregunta..."
-                            label="Pregunta"
-                            error="Tu pregunta debe llevar más de 10 caracteres."
-                            regularExpresion={/^[a-zA-ZÀ-ÿ\s?.,0-9]{4,100}$/}
-                        />
-                    </NameDiv>
-
-                    <InputForm
-                        // type="url"
-                        state={code}
-                        changeState={setCode}
-                        name="code"
-                        placeholder="Imagen de tu código..."
-                        label="Imagen de código"
-                        error="Muéstranos una imagen sobre tu problema."
-                        regularExpresion={/(https?:\/\/.*\.(?:png|jpg))/i}
-                    />
-                    <InputFormArea
-                        // type="textarea"
-                        state={description}
-                        changeState={setDescription}
-                        name="description"
-                        placeholder="Describe tu pregunta..."
-                        label="Descripción"
-                        error="Háblanos sobre tu problema"
-                        regularExpresion={/^[a-zA-ZÀ-ÿ\s?.,0-9]{20,400}$/}
-                    />
-
-                </div>
-
-
-
-
-                <div className={Classes.selectModules}>
-                    <label>
-                        Module selected <span className={Classes.slec}>{renderResult()} </span>
-                        <select
-                            className={Classes.selectInput}
-                            onChange={handleOnChange}
-                            value={moduleSelected}
-                            name='field'
-                        >
-                            <option value={'selectModule'}>MODULES</option>
-                            <option value={'m1'}>M1</option>
-                            <option value={'m2'}>M2</option>
-                            <option value={'m3'}>M3</option>
-                            <option value={'m4'}>M4</option>
-                        </select>
-                    </label>
-
-                </div>
-
-
-                {m1TagsSelected &&
-                    <FormM1Tags
-                        setTag={setTag}
-                        tag={tag}
-                    />}
-                {m2TagsSelected &&
-                    <FormM2Tags
-                        setTag={setTag}
-                        tag={tag}
-                    />}
-                {m3TagsSelected &&
-                    <FormM3Tags
-                        setTag={setTag}
-                        tag={tag}
-                    />}
-                {m4TagsSelected &&
-                    <FormM4Tags
-                        setTag={setTag}
-                        tag={tag}
-                    />}
-
-
-                <button className={Classes.button} type='submit'>Enviar</button>
-
-                {
-                    validate === true &&
-                    <Success>Successfully saved, redirecting to home</Success>
-                }
-
-            </Formulario>
-
-        </div >
-    );
+        {validate === true && (
+          <Success>Successfully saved, redirecting to home</Success>
+        )}
+      </Formulario>
+    </div>
+  );
 };
 
 export default PostFormMui;
 
-
 const colores = {
-    inputPurple: "#413A66",
-    error: "#f66060",
-    succes: "#71ff4a"
-}
+  inputPurple: "#413A66",
+  error: "#f66060",
+  succes: "#71ff4a",
+};
 
 const Formulario = styled.form`
-    display: flex;
-    flex-direction: column;
-    width: 580px;
-    /* grid-template-columns: 1fr 1fr; */
-    /* gap: 20px; */
-    align-items: center;
-    text-align: center;
-    margin: 10px;
-    padding: 20px;
-    background-color: white;
-    border-radius:  35px ;
-    border: solid 1px black;
-    /* box-shadow: 0px 0px 20px rgba(14, 29, 65,0.2); */
+  display: flex;
+  flex-direction: column;
+  width: 580px;
+  /* grid-template-columns: 1fr 1fr; */
+  /* gap: 20px; */
+  align-items: center;
+  text-align: center;
+  margin: 10px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 35px;
+  border: solid 1px black;
+  /* box-shadow: 0px 0px 20px rgba(14, 29, 65,0.2); */
+  margin-left: 20px;
+  margin-right: 20px;
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr;
     margin-left: 20px;
     margin-right: 20px;
-    @media (max-width: 800px){
-        grid-template-columns: 1fr;
-        margin-left: 20px;
-        margin-right: 20px;
-    }
-`
+  }
+`;
 
 const Description = styled.textarea`
-/* display: flex; */
-/* align-items: center; */
-/* text-align: center; */
+  /* display: flex; */
+  /* align-items: center; */
+  /* text-align: center; */
 
-margin-top:10px;
-flex-direction: column;
-width: 78%;
-border-radius: 4px;
-resize: none; 
-border: 2px solid grey ;
-
-
-`
-
+  margin-top: 10px;
+  flex-direction: column;
+  width: 78%;
+  border-radius: 4px;
+  resize: none;
+  border: 2px solid grey;
+`;
 
 const Success = styled.p`
-    font-size: 20px;
-    background: ${colores.succes};
-    color: #141414;
-    padding: 15px 30px;
-    font-weight: bold;
-    border-radius: 5px;
-`
+  font-size: 20px;
+  background: ${colores.succes};
+  color: #141414;
+  padding: 15px 30px;
+  font-weight: bold;
+  border-radius: 5px;
+`;
