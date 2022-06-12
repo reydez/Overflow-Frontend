e0b2f5065d5a6141184066248c6fe34bf62be68d
Ayilk <srangel.h92@gmail.com>
2022-06-12 17:07:48 -0500

    Info planes

--- src/Controllers/constante.js
+++ src/Controllers/constante.js
@@ -31,6 +31,14 @@ const ButtonLogOut = () => {
 
   return (
     <>
+
+        <Link to='/donar' style={{ textDecoration: "none" }}>
+          <Button sx={{ color: "#7165A0;", "&:hover": { color: "#F50057" } }}>
+            <FavoriteIcon sx={{ marginRight: "10px", fontSize: "18px" }} />
+            Danos Amor $$
+            <FavoriteIcon sx={{ marginRight: "10px", fontSize: "18px" }} />
+          </Button>
+        </Link>,
       {user.isAdmin === true ? (
         <Link to="/admin" style={{ textDecoration: "none" }}>
           <Button
@@ -52,13 +60,7 @@ const ButtonLogOut = () => {
         </button>
       </ButtonLogOutDiv>
       
-      <Link to='/donar' style={{ textDecoration: "none" }}>
-          <Button sx={{ color: "#7165A0;", "&:hover": { color: "#F50057" } }}>
-            <FavoriteIcon sx={{ marginRight: "10px", fontSize: "18px" }} />
-            Danos Amor $$
-            <FavoriteIcon sx={{ marginRight: "10px", fontSize: "18px" }} />
-          </Button>
-        </Link>,
+      
       
     </>
   );
